5dc9de958e204f299de1b228c716b4a21f86361d
sagar3595 <sagar47kumar13@gmail.com>
2023-09-24 21:28:38 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,28 @@
-import '@/styles/globals.css'
+import { createGlobalStyle } from "styled-components"
+import { Inter } from 'next/font/google'
+import { CartContextProvider } from "@/components/CartContext";
+const GlobalStyles = createGlobalStyle`
+/* @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap'); */
+body{
+  background-color: #eee;
+  padding: 0;
+  margin: 0;
+  /* font-family: 'Poppins', sans-serif; */
+}
+`;
+
+const inter = Inter({ subsets: ['latin'] })
+ 
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <main className={inter.className}>
+    <CartContextProvider>
+  <GlobalStyles />
+  <Component {...pageProps} />
+  </CartContextProvider>
+  
+  </main>
+  );
 }
